0a93bf859a2b77fb7fff3fad0efb69b15026b6c1
krishnaprasad <krishnaprasad8369@gmail.com>
2024-01-28 17:04:13 +0530

    functions added

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -1,41 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Todo.css";
 import { FaPlus } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 const Todo = () => {
+  const [input, setinput] = useState("");
+  const [addButtton, setAddButtton] = useState([]);
+
+  const handleinput = (e) => {
+    setinput(e.target.value);
+  };
+  const handleInputButton = () => {
+    setAddButtton([...addButtton, input]);
+    setinput('')
+  };
+  const handleDelete=(index)=>{
+setAddButtton(addButtton.filter((data,dataindex)=>dataindex!=index))
+  }
   return (
     <>
       <main>
         <section className="main-section">
-          <div>
-            
-          </div>
+          {/* tttle */}
           <div className="title">
             <h2>TODO LIST</h2>
             <h3>Have a good day🌝 ☕ </h3>
           </div>
-
+          {/*input section  */}
           <div className="input-section">
-            <input type="text" />
-            <i>
+            <input value={input} type="text" onChange={handleinput} />
+            <i onClick={handleInputButton}>
               <FaPlus />
             </i>
           </div>
+          {/* article section */}
           <section>
-          <div className="todo">
-            <div className="todo-input">
-           <article>
-
-           </article>
-              <i><MdDelete /></i>
-
+            <div className="todo">
+              {
+                addButtton.map((data,index)=>((
+                  <div className="todo-input">
+                  <article>{data}</article>
+  
+                  <i onClick={()=>handleDelete(index)}>
+                    <MdDelete />
+                  </i>
+                </div>
+                )))
+              }
+           
             </div>
-
-          </div>
+          </section>
         </section>
-         
-        </section>
-        
       </main>
     </>
   );
